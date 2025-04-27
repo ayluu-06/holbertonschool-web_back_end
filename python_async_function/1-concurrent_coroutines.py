d603780ef4d8aeebf4e7697799f6942a989614f7
ayluu-06 <pintosaylin7@gmail.com>
2025-04-27 20:46:37 -0300

    execute multiple coroutines

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -9,7 +9,7 @@ from bisect import insort
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n: int, max_delay: int = 10) -> List[float]:
+async def wait_n(n: int, max_delay: int) -> List[float]:
     """documented"""
     tasks = [asyncio.create_task(wait_random(max_delay)) for _ in range(n)]
     delays: List[float] = []
