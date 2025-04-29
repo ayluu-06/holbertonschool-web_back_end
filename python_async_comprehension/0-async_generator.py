c3f1124be00be12497d7717b5b9f9e2aa42fcf1e
ayluu-06 <pintosaylin7@gmail.com>
2025-04-29 12:23:05 -0300

    Async Generator

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -2,9 +2,10 @@
 """0-async_generator.py"""
 import asyncio
 import random
+from typing import AsyncGenerator
 
 
-async def async_generator():
+async def async_generator() -> AsyncGenerator[float, None]:
     """documented"""
     for _ in range(10):
         await asyncio.sleep(1)
