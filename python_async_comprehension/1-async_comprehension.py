202f31f676cef30b0d3e17679cb107c145f8fa2e
ayluu-06 <pintosaylin7@gmail.com>
2025-04-29 14:53:31 -0300

    Async Comprehensions

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -8,4 +8,5 @@ async_generator = __import__('0-async_generator').async_generator
 
 
 async def async_comprehension() -> List[float]:
+    """documented"""
     return [num async for num in async_generator()]
