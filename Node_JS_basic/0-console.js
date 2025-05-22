0f6e013235fe2b42081a17a48b442a50011a0403
ayluu-06 <pintosaylin7@gmail.com>
2025-05-22 20:13:14 -0300

    Executing basic javascript with Node JS

--- Node_JS_basic/0-console.js
+++ Node_JS_basic/0-console.js
@@ -1,6 +1,6 @@
 // 0-console.js
 function displayMessage(message) {
-    console.log(message);
-  }
-  
-  module.exports = displayMessage;
+  console.log(message);
+}
+
+module.exports = displayMessage;
