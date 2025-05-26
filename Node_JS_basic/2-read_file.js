b69c2a17051edf13dd8e78591d40fa41f0614fd9
ayluu-06 <pintosaylin7@gmail.com>
2025-05-25 21:50:05 -0300

    Reading a file synchronously with Node JS

--- Node_JS_basic/2-read_file.js
+++ Node_JS_basic/2-read_file.js
@@ -30,7 +30,7 @@ function countStudents(path) {
       console.log(`Number of students in ${field}: ${names.length}. List: ${names.join(', ')}`);
     });
   } catch (error) {
-    console.error('Cannot load the database');
+    throw new Error('Cannot load the database');
   }
 }
 
